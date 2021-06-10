3c14c7fbad538076b0641bb331128d31dde4c103
feketebence <feketebencetyping@gmail.com>
2021-06-11 01:20:02 +0300

    SocketIO based client-server communication setup + minor cleanup

--- app.py
+++ app.py
@@ -1,37 +1,33 @@
-from flask import Flask, request, render_template
-
-from nltk.corpus.reader.chasen import test
-import pandas as pd
-from nltk.corpus import stopwords, util
-
-import util.text_process as tp
-import util.utils as utils
-
 from core import process_user_input
 from core import put_row_into_csv
 
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_socketio import emit
+
 app = Flask(__name__)
+app.debug = True
+socketio = SocketIO(app)
+csv_path = 'static/event_coords_data.csv'
+
+@socketio.on('conn_event')
+def handle_message(data):
+    print('\t[ Recieved message:', data, ']')
+
+@socketio.on('user_input')
+def handle_user_input(text):
+    print("\t[ Got data from clientside: " + text + " ]")
+    d = process_user_input(text)
+    
+    print('\t[ Processed result:', d, ']')
+    put_row_into_csv(csv_path, d)
+
+    emit('redraw', {'q': 'q'})
+    print('\t[ Sent redraw request to client ]')
 
 @app.route('/')
-
 def my_form():
     return render_template('map.html')
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    csv_path = 'static/event_coords_data.csv'
-    text = request.form['text']
-    print("\t[ Got string from front-end:", text, ']')
-
-    d = process_user_input(text)
-
-    print('\t[ Processed result:', d, ']')
-
-    data_as_csv = put_row_into_csv(csv_path, d)
-    print("\t[ Appended the following to " + csv_path + ":" + data_as_csv)
-
-    return str(data_as_csv)
     
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    socketio.run(app)
--- static/map.js
+++ static/map.js
@@ -1,89 +1,84 @@
-/*var margin = {top: 1, right: 1, bottom: 1, left: 1},
-            width = 980 - margin.left - margin.right,
-            height = 450 - margin.top - margin.bottom;*/
-        var width=600, height=500;
-        
-        d3.queue()
-        .defer(d3.json,"https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/romania.geojson")
-        .defer(d3.csv, "static/event_coords_data.csv")
-        .await(ready)
-        
-        var canvas = d3.select("#map").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("class","canvas")
-        .call(d3.zoom().on("zoom",function(){
-            canvas.attr("transform", d3.event.transform).transition().duration(100)
-        }))
-        .append("g");
-        
-        var data1=d3.json("https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/romania.geojson")
-        var data2=d3.csv("static/event_coords_data.csv")
-        
-        //var scale = 400;
-        //var projection = d3.geoMercator().scale(scale).translate([width / 2, height / 2]).attr("id","project");
-        var projection = d3.geoMercator().center([2, 47]).scale(2000).translate([width / 1.5, height / 1.5]);//.attr("cx", 50).attr("cy", 90);
+var width=innerWidth - 300, height=innerHeight - 200;
 
-        d3.select("#project").attr("align","center");
-        
-        console.log(data2);
-        
-        function ready(error, data1, data2){
-            console.log(data2);
-            var g = canvas.selectAll("g")
-            .data(data1.features)
-            .enter();
-            
-            var path = d3.geoPath().projection(projection);
-            
-            var areas = g.append("path")
-            .attr("d", path)
-            .attr("class", "area")
-            .attr("fill", "#8FBC8F")
-            .attr("stroke", "black")
-            .attr("stroke-width",.4);
-            
-            g = canvas.append("g");
-            
-             var tip = d3.tip()
-                .attr("class", "d3-tip")
-                .offset([-10, 0])
-                .html(function(d) {
-                    
-                   return "Város: " + d.city_name + "<br>" + "Esemény: " + d.event + "<br>" ;
-                    
-                });
-                    
-            canvas.call(tip);
-            
-            g.selectAll(".city-circle")
-            .data(data2)
-            .enter()
-            .append("circle")
-            .attr("fill", "red")
-            .attr("stroke","black")
-            .attr("r", 3)
-            .attr("cx", function(d){
-                var coordinates = projection([d.x_coord, d.y_coord])
-                return coordinates[0];
-            })
-            .attr("cy", function(d){
-                var coordinates = projection([d.x_coord, d.y_coord])
-                return coordinates[1];
-            })
-            
-            .on("mouseover",tip.show).attr("cursor","crosshair")
-            .on("mouseout", tip.hide);
+d3.queue()
+.defer(d3.json,"https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/romania.geojson")
+.defer(d3.csv, "static/event_coords_data.csv")
+.await(ready)
 
-            var tip = d3.tip()
-            .attr("class", "d3-tip")
-            .offset([-10, 0])
-            .html(function(d) {
-                
-               return "Város: " + d.city_name + "<br>" + "Esemény: " + d.event + "<br>" ;
-                
-            });
-                
+var canvas = d3.select("#map").append("svg")
+.attr("width", width)
+.attr("height", height)
+.attr("class","canvas")
+.call(d3.zoom().on("zoom",function(){
+    canvas.attr("transform", d3.event.transform).transition().duration(100)
+}))
+.append("g");
 
-            canvas.call(tip);
-        }
+var data1 = d3.json("https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/romania.geojson")
+var data2 = d3.csv("static/event_coords_data.csv")
+
+//var scale = 400;
+//var projection = d3.geoMercator().scale(scale).translate([width / 2, height / 2]).attr("id","project");
+var projection = d3.geoMercator().center([2, 47]).scale(2000).translate([width / 1.5, height / 1.5]);//.attr("cx", 50).attr("cy", 90);
+
+d3.select("#project").attr("align","center");
+
+function ready(error, data1, data2){
+    console.log(data2);
+    var g = canvas.selectAll("g")
+    .data(data1.features)
+    .enter();
+    
+    var path = d3.geoPath().projection(projection);
+    
+    var areas = g.append("path")
+    .attr("d", path)
+    .attr("class", "area")
+    .attr("fill", "#8FBC8F")
+    .attr("stroke", "black")
+    .attr("stroke-width",.4);
+    
+    g = canvas.append("g");
+    
+        var tip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([-10, 0])
+        .html(function(d) {
+            
+            return "Város: " + d.city_name + "<br>" + "Esemény: " + d.event + "<br>" ;
+            
+        });
+            
+    canvas.call(tip);
+    
+    g.selectAll(".city-circle")
+    .data(data2)
+    .enter()
+    .append("circle")
+    .attr("fill", "red")
+    .attr("stroke","black")
+    .attr("r", 3)
+    .attr("cx", function(d){
+        var coordinates = projection([d.x_coord, d.y_coord])
+        return coordinates[0];
+    })
+    .attr("cy", function(d){
+        var coordinates = projection([d.x_coord, d.y_coord])
+        return coordinates[1];
+    })
+    
+    .on("mouseover",tip.show).attr("cursor","crosshair")
+    .on("mouseout", tip.hide);
+
+    var tip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([-10, 0])
+    .html(function(d) {
+        
+        return "Város: " + d.city_name + "<br>" + "Esemény: " + d.event + "<br>" ;
+        
+    });
+        
+
+    canvas.call(tip);
+}
